c4635037130bfcc48b3fd0da0864ee797a83cbf9
ashwanycs0-maker <ashwanycs0@gmail.com>
2026-06-23 15:52:58 +0530

    Delete ai_anomaly.py

--- ai_anomaly.py
+++
@@ -1,9 +0,0 @@
-import random
-from analyzer.risk_engine import calculate_risk
-
-def detect_anomaly(device, value):
-
-    value = random.randint(0,100)
-
-    if value > 70:   # 🔥 lower for demo
-        calculate_risk("Anomaly Detected", device, value)
